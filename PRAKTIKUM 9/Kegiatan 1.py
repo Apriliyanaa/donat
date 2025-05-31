bead2d4c7df71524d49ec2e357550f0861db87ec
Apriliyanaa <L200240054@student.ums.ac.id>
2025-05-31 11:39:48 +0700

    Delete PRAKTIKUM 9 directory

--- PRAKTIKUM 9/Kegiatan 1.py
+++
@@ -1,4 +0,0 @@
-list = ["Nim : L200240054 \n","Tanggal_lahir : 20-04-2006 \n","Nama : Erbita Putri Apriliyana \n"]
-file = open("L200240054","w")
-file.writelines(list)
-file.close()
--- PRAKTIKUM 9/Kegiatan 2.py
+++
@@ -1,9 +0,0 @@
-list = ["Nim : L200240054 \n","Tanggal_lahir : 20/04/2006 \n","Nama : Erbita Putri Apriliyana \n"]
-file = open("L200240054","r")
-all_lines = file.readlines()
-print(all_lines[2])
-print(all_lines[3], end=", ")
-print(all_lines[1])
-print(all_lines[0])
-file.close()
-
